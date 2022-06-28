3ff215b2ca3715b3d951a67881100364e6c1f366
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-06-28 17:24:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,5 +13,34 @@ function gotResults(error, results) {
         console.log(error);
     } else {
         console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) +1;
+        random_number_g = Math.floor(Math.random() * 255) +1;
+        random_number_b = Math.floor(Math.random() * 255) +1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img1  = document.getElementById('Animals');
+
+        if (results[0].label == "Meowing") {
+            img1.src = 'cat.jpg' ;
+        }
+        else if (results[0].label == "Mooing") {
+            img1.src = 'Cow.jpg' ;
+        }
+        else if (results[0].label == "Roaring") {
+            img1.src = 'lion1.jpg' ;
+        }
+        else if (results[0].label == "Barking") {
+            img1.src = 'Dog.png' ;
+        }
+        else  {
+            img1.src = 'aliens-01.png' ;
+            img2.src = 'aliens-02.png';
+            img3.src = 'aliens-03.png';
+            img4.src = 'aliens-04.gif';
+        }
     }
 }
